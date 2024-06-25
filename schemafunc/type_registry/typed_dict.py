dff78d7cae7e5fde6627b99872eaa908d16f51af
Dustin Wyatt <dustin.wyatt@gmail.com>
2024-06-25 13:52:50 -0500

    Update type hint retrieval in typed_dict
    The way type hints are retrieved in 'typed_dict.py' is updated. Instead of directly accessing the '__annotations__' dictionary, the 'get_type_hints' function from Python's 'typing' library is used. This change contributes to better error handling and resilience in the type hint retrieval process.

--- schemafunc/type_registry/typed_dict.py
+++ schemafunc/type_registry/typed_dict.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Type, TypedDict, _TypedDictMeta
+from typing import Any, Dict, Type, TypedDict, _TypedDictMeta, get_type_hints
 
 from schemafunc.type_registry.registry import (
     TypeHandler,
@@ -15,7 +15,7 @@ class TypedDictTypeHandler(TypeHandler):
     def resolve(self, param_type: Type[TypedDict]) -> Dict[str, Any]:
         properties = {}
         required = []
-        for field_name, field_type in param_type.__annotations__.items():
+        for field_name, field_type in get_type_hints(param_type).items():
             properties[field_name] = resolve_type(field_type)
             if field_name in param_type.__required_keys__:
                 required.append(field_name)
